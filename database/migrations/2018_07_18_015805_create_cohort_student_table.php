cb84e9f89b4bcb4f60ab7c84d7c671349737b588
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-18 01:58:37 +0000

    CohortStudent migration

---
+++ database/migrations/2018_07_18_015805_create_cohort_student_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCohortStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cohort_student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('cohort_id');
+            $table->unsignedInteger('student_id');
+            $table->timestamps();
+
+            $table->foreign('cohort_id')
+                ->references('id')
+                ->on('cohorts')
+                ->onDelete('cascade');
+            $table->foreign('student_id')
+                ->references('id')
+                ->on('students')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cohort_student');
+    }
+}
