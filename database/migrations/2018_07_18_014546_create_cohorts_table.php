a46e55845395e1662826e17f80236730a446695e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-18 01:55:09 +0000

    Cohort migration and model

---
+++ database/migrations/2018_07_18_014546_create_cohorts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCohortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cohorts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('institution_id');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('institution_id')
+                ->references('id')
+                ->on('institutions')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cohorts');
+    }
+}
