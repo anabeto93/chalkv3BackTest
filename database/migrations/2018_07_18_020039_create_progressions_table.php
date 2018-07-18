783b7d0ec979fff258dc6f4591dfdd6e4ddefb73
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-18 02:01:41 +0000

    Progression migration and model

---
+++ database/migrations/2018_07_18_020039_create_progressions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProgressionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('progressions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('session_id');
+            $table->unsignedInteger('student_id');
+            $table->timestamps();
+
+            $table->foreign('session_id')
+                ->references('id')
+                ->on('sessions')
+                ->onDelete('cascade');
+            $table->foreign('student_id')
+                ->references('id')
+                ->on('students')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('progressions');
+    }
+}
