e52e051e4eeaf57acca08cf8aa46523b1e748d70
Appier-Sign <braasig@gmail.com>
2018-07-19 12:51:55 +0000

    private properties added to Admin model

--- app/Admin.php
+++ app/Admin.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
+    private $username;
+    private $password;
+
+    protected $fillable = ['username', 'password'];
+
     /**
      * The Institutions that belong to the Admin.
      */
@@ -13,8 +18,18 @@ class Admin extends Model
         return $this->belongsToMany(Institution::class);
     }
 
+    /**
+     * Change username to lower case
+     *
+     * @param $username
+     */
     public function setUserNameAttribute($username)
     {
         $this->attributes['username'] = strtolower($username);
+    }
+
+    public function store()
+    {
+
     }
 }
