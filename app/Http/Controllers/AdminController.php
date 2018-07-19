ff1ea9ca41503e789b0cf42bfb2047bab2e1b9a8
Appier-Sign <braasig@gmail.com>
2018-07-19 10:06:27 +0000

    StoreAdmin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAdminRequest;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -32,9 +33,9 @@ class AdminController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreAdminRequest $request)
     {
-        //
+        return response($request->all());
     }
 
     /**
