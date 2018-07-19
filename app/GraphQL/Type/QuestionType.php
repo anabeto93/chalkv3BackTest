5501f4d6fb9e253cd7253d5c5a934786c51c7871
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-19 04:35:33 +0000

    Added QuestionType and fixed HasUpdatesType

---
+++ app/GraphQL/Type/QuestionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+use App\Question;
+
+class QuestionType extends GraphQLType {
+	protected $attributes = [
+		'name' => 'Question',
+		'description' => 'A Question',
+		'model' => Question::class,
+	];
+
+	public function fields() {
+		return [
+			'id' => [
+				'type' => Type::nonNull(Type::int()),
+				'description' => 'Question ID',
+			],
+			'title' => [
+				'type' => Type::nonNull(Type::string()),
+				'description' => 'Title of the question',
+            ],
+			'feedback' => [
+				'type' => Type::string(),
+				'description' => 'Feedback of the question',
+            ],
+			'answers' => [
+				'type' => Type::listOf(GraphQL::type('question_answer')),
+				'description' => 'Answers of the question',
+            ]
+		];
+	}
+}
