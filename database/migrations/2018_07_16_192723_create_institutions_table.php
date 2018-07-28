2e79666c8aa1962db3e3bffe992b2ddabb1ad40e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-28 02:27:51 +0000

    Added default hex color code for Institutions

--- database/migrations/2018_07_16_192723_create_institutions_table.php
+++ database/migrations/2018_07_16_192723_create_institutions_table.php
@@ -16,7 +16,7 @@ class CreateInstitutionsTable extends Migration
         Schema::create('institutions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->char('color', 7);
+            $table->char('color', 7)->default('#d8497d');
             $table->timestamps();
         });
     }
