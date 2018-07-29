051fb8a40965a469f6b5f0006562a1e4b9311b66
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-29 16:40:28 +0000

    ProgressionsTableSeeder added

---
+++ database/seeds/ProgressionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Progression;
+use Illuminate\Database\Seeder;
+
+class ProgressionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Progression::create([
+            
+        ]);
+    }
+}
