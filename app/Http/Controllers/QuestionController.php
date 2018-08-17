456649d2bbe88c79f996f2c5e44abf58b046dfe8
Appier-Sign <braasig@gmail.com>
2018-08-17 06:00:54 +0000

    setters of attribute of Question defined. index() returns list of questions

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -13,7 +14,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Question::all()->toArray());
     }
 
     /**
--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function setQuizId(int $quiz)
+    {
+        $this->quiz()->associate($quiz);
+        return $this;
+    }
+
+    public function setType(string $type)
+    {
+        $this->attributes['type'] = $type;
+        return $this;
+    }
+
+    public function setFeedback(string $feedback)
+    {
+        $this->attributes['feedback'] = $feedback;
+        return $this;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->attributes['title'] = $title;
+        return $this;
+    }
     /**
      * The Quiz the Question belongs to.
      */
