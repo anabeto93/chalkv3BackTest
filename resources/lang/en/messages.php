865fdd73499f8d141e7cf23ceee8c6ea5031c68e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-17 15:12:36 +0000

    Base translation files

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Messages used multiple times throughout the views are stored here
+    |
+    */
+    'title' => [
+        'institutions' => 'Institutions',
+        'admins' => 'Admins',
+        'users' => 'Users',
+        'cohorts' => 'Cohorts',
+        'courses' => 'Courses',
+        'folders' => 'Folders',
+        'sessions' => 'Sessions',
+        'quizzes' => 'Quizzes',
+        'questions' => 'Questions',
+        'questionAnswers' => 'Answers',
+        'progressions' => 'User Progressions',
+    ],
+
+    'list' => [
+        'institutions' => 'List of Institutions',
+        'admins' => 'List of Admins',
+        'users' => 'List of Users',
+        'cohorts' => 'List of Cohorts',
+        'courses' => 'List of Courses',
+        'folders' => 'List of Folders',
+        'sessions' => 'List of Sessions',
+        'quizzes' => 'List of Quizzes',
+        'questions' => 'List of Questions',
+        'questionAnswers' => 'List of Answers',
+        'progressions' => 'User Progressions',
+    ],
+
+    'none' => [
+        'none.institutions' => 'No Institutions found!',
+        'admins' => 'No Admins found!',
+        'users' => 'No Users found!',
+        'cohorts' => 'No Cohorts found!',
+        'courses' => 'No Courses found!',
+        'folders' => 'No Folders found!',
+        'sessions' => 'No Sessions found!',
+        'quizzes' => 'No Quizzes found!',
+        'questions' => 'No Questions found!',
+        'questionAnswers' => 'No Answers found!',
+        'progressions' => 'No users have progressed past this point!',
+    ],
+    
+];
