3d489e6ee6ee36cea2c5e93bd551f7596093498c
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-17 15:14:05 +0000

    Modified navbar

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,15 +1,23 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        @auth('admin')
+{{--        @auth('admin')--}}
             <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('courses') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('courses.list') }}">
+                            @lang('messages.title.courses')
+                        </a>
+                        {{--<a class="nav-link" href="{{ route('folders.list') }}">--}}
+                            {{--@lang('messages.title.folders')--}}
+                        {{--</a>--}}
+                        {{--<a class="nav-link" href="{{ route('sessions.list') }}">--}}
+                            {{--@lang('messages.title.sessions')--}}
+                        {{--</a>--}}
                     </li>
                 </ul>
 
@@ -17,7 +25,7 @@
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::admin()->name }} <span class="caret"></span>
+{{--                            {{ Auth::admin()->name }} <span class="caret"></span>--}}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -33,7 +41,7 @@
                         </div>
                     </li>
                 </ul>
-            @endauth
+            {{--@endauth--}}
         </div>
     </div>
 </nav>
