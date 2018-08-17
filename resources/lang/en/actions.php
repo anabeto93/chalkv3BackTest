52fc5535a8275aec0b04a071324686e1b9f5a868
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-17 15:12:36 +0000

    Base translation files

---
+++ resources/lang/en/actions.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Names of actions in views
+    |
+    */
+
+    'create' => [
+        'title' => 'Create',
+        'institution' => 'Create Institution',
+        'admin' => 'Create Administrator',
+        'course' => 'Create Course',
+        'user' => 'Create User',
+        'cohort' => 'Create Cohort',
+        'folder' => 'Create Folder',
+        'session' => 'Create Session',
+        'quiz' => 'Create Quiz',
+        'question' => 'New Question',
+        'questionAnswer' => 'Add Answer',
+    ],
+    
+    'edit' => [
+        'title' => 'Edit',
+        'institution' => 'Edit Institution',
+        'admin' => 'Edit Administrator',
+        'course' => 'Edit Course',
+        'user' => 'Edit User',
+        'cohort' => 'Edit Cohort',
+        'folder' => 'Edit Folder',
+        'session' => 'Edit Session',
+        'quiz' => 'Edit Quiz',
+        'question' => 'Edit Question',
+        'questionAnswer' => 'Edit Answer',
+    ],
+    
+    'delete' => [
+        'title' => 'Delete'
+    ],
+    'remove' => 'Remove',
+
+    'view' => [
+        'title' => 'View'
+    ],
+    'preview' => 'Preview',
+
+    'save' => 'Save',
+    'update' => 'Update'
+
+];
---
+++ resources/lang/en/fields.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fields Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Names of actions in views
+    |
+    */
+
+    'name' => 'Name',
+    'description' => 'Description',
+    'teacher' => 'Teacher',
+    'enabled' => 'Enabled',
+    'actions' => 'Actions'
+
+];
