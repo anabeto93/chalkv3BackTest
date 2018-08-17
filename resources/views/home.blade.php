c239b06c8ee5d45fbe2e165e2fab2c41afcece4e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-17 17:58:39 +0000

    Modified home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.base')
 
+@section('title')
+    @lang('messages.title.dashboard')
+@endsection
+
 @section('content')
-Welcome to your Dashboard.
+    Welcome to your Dashboard.
 @endsection
