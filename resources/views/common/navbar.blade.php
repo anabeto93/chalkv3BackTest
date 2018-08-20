1b280e0b70c7e2c7f0d72ed5a71faca02e11d142
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-20 19:36:01 +0000

    Navbar

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -9,15 +9,24 @@
             <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('courses.list') }}">
+                        <a class="nav-link" href="{{ route('course.list') }}">
                             @lang('messages.title.courses')
                         </a>
-                        {{--<a class="nav-link" href="{{ route('folders.list') }}">--}}
-                            {{--@lang('messages.title.folders')--}}
-                        {{--</a>--}}
-                        {{--<a class="nav-link" href="{{ route('sessions.list') }}">--}}
-                            {{--@lang('messages.title.sessions')--}}
-                        {{--</a>--}}
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.list') }}">
+                            @lang('messages.title.users')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('cohort.list') }}">
+                            @lang('messages.title.cohorts')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('quiz.list') }}">
+                            @lang('messages.title.quizzes')
+                        </a>
                     </li>
                 </ul>
 
@@ -25,14 +34,16 @@
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            Admin
 {{--                            {{ Auth::admin()->name }} <span class="caret"></span>--}}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                document.getElementById('logout-form').submit();">
+                                <i width="16" height="16" data-feather="log-out"></i>
+                                @lang('actions.logout')
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
