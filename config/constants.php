600ee7ffa36ad742299dd1ca64c1e119493994b0
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-21 20:03:47 +0000

    Added quiz type constants

--- config/constants.php
+++ config/constants.php
@@ -4,5 +4,10 @@ return [
     'lang' => [
         'en' => 'English',
         'fr' => 'French'
-    ]
+    ],
+
+    'quiz_type' => [
+        'short_exact' => 'Short Exact Answer',
+        'multiple_choice' => 'Multiple Choice Answers',
+    ],
 ];
