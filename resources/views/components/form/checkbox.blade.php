0a3837afb665be309d7f6a1a6e7f10c51f73a13c
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-21 20:04:35 +0000

    Extra param to bootstrap checkbox

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -1,4 +1,5 @@
 <div class="form-group form-check">
-    {{ Form::checkbox($name, $value, null, array_merge(['id' => $name, 'class' => 'form-check-input'], $attributes)) }}
+    {{ Form::checkbox($name, $value, $checked, array_merge(['id' => $name, 'class' => 'form-check-input'], $attributes)
+    ) }}
     {{ Form::label($name, null, ['class' => 'form-check-label']) }}
 </div>
