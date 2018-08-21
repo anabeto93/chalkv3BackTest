b3c38e6268ddf4e21978f287ab6a2d0137eb331e
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-21 04:25:18 +0000

    Fixed attribute merging with custom components

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -1,5 +1,5 @@
 {{ Form::label($name, null, []) }}
 <div class="form-group custom-file">
-    {{ Form::file($name, ['class' => 'custom-file-input']) }}
+    {{ Form::file($name, array_merge(['class' => 'custom-file-input'], $attributes)) }}
     {{ Form::label($name, $value, ['class' => 'custom-file-label']) }}
 </div>
