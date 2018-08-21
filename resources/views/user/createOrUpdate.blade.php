11b117f30e06d6e7dccbfe14f98c204e3894e085
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-08-21 17:39:22 +0000

    Fixed country and language dropdown

--- resources/views/user/createOrUpdate.blade.php
+++ resources/views/user/createOrUpdate.blade.php
@@ -13,6 +13,7 @@
     {{ Form::bsText('first_name', Input::old('first_name'), ['required' => true]) }}
     {{ Form::bsText('last_name', Input::old('last_name'), ['required' => true]) }}
     {{ Form::bsText('phone_number', Input::old('phone_number'), ['required' => true]) }}
-    {{ Form::bsSelect('country', Countries::getList(app()->getLocale()), Input::old('country'), ['required' => true]) }}
+    {{ Form::bsSelect('country', Countries::getList(app()->getLocale()), Input::old('country'), ['required' =>
+    true]) }}
     {{ Form::bsSelect('language', Config::get('constants.lang'), Input::old('language'), ['required' => true]) }}
 @endsection
